ff92923948a9a6c0d36b658444600b3ac0117273
Andre Luis <DudaRosa>
2019-10-20 11:58:35 -0300

    ajustes cupom e noticia

--- src/main/java/br/com/fmu/repository/NoticiaRepository.java
+++ src/main/java/br/com/fmu/repository/NoticiaRepository.java
@@ -15,7 +15,7 @@ public interface NoticiaRepository extends CrudRepository<Noticia, Integer> {
 	@Query(value = "SELECT * FROM dbo.noticia", nativeQuery = true)
 	List<Noticia> findAll();
 	
-	@Query(value = "SELECT * FROM dbo.noticia WHERE dia LIKE :dia%", nativeQuery = true)
+	@Query(value = "SELECT * FROM dbo.noticia WHERE DAY(CAST((dia) AS DATE)) = :dia", nativeQuery = true)
 	List<Noticia> findbyDia(String dia);
 	
 
